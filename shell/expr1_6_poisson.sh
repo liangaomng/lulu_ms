65043299a9246a63affb1f86de9dd62000871df7
liangaomng <liangaoming@foxmail.com>
2024-01-25 21:06:34 +0800

    1/25/1

--- shell/expr1_6_poisson.sh
+++ shell/expr1_6_poisson.sh
@@ -5,15 +5,15 @@ export DDE_BACKEND=pytorch
 export pde_task=Poisson
 export sub_folder=Expr2d_1_6
 
-for name_order in {1..1}; do
+for name_order in {1..10}; do
 
    export expr_floder="EXPRS/$pde_task/$sub_folder/Poisson_Seed_Scale_Expr1_6_$name_order"
 
-   for seed in {1..1};do
+   for seed in {1..3};do
 
       echo $expr_floder/Expr_$seed.xlsx 
       
-      python Expr1_run.py --expr_set_path $expr_floder/Expr_$seed.xlsx --pde_name "$pde_task" --mu 15.0 --epoch 10000
+      python Expr1_run.py --expr_set_path $expr_floder/Expr_$seed.xlsx --pde_name "$pde_task" --mu 15.0 --epoch 50
       echo "expr: done seed:$seed  order:$name_order"
 
    done
